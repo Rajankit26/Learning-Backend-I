4aec80dfd0c2dec2f8471af9a0dc47aa708530b3
Ankit Raj <142731001+Rajankit26@users.noreply.github.com>
2025-03-01 21:05:06 +0000

    Added AWS S3 file upload and delete functionality

--- src/config/index.js
+++ src/config/index.js
@@ -6,6 +6,10 @@ const config = {
     PORT : process.env.PORT || 5000,
     MONGODB_URL : process.env.MONGODB_URL || 'mongodb://localhost:27017/e-comm',
     JWT_SECRET : process.env.JWT_SECRET || "secretkey",
-    JWT_EXPIRY : process.env.JWT_EXPIRY || "15d"
+    JWT_EXPIRY : process.env.JWT_EXPIRY || "15d",
+    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+    S3_REGION: process.env.S3_REGION,
 }
 export default config
